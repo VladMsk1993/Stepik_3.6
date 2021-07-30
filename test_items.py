6299b24c677e3396655b39df8e791f06cccc520f
VladMSK1993 <sharvladrus@gmail.com>
2021-07-30 15:48:14 +0300

    Доделан файл, скрипт работает полностью, корректно.

--- test_items.py
+++ test_items.py
@@ -1,11 +1,11 @@
-import pytest
-from selenium import webdriver
-from conftest import *
+import time
 
 
-@pytest.fixture(scope="function")
-def aproving_button(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+def test_guest_see_button_text(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    button = browser.find_element_by_id("id_quantity")
-    button.click()
+    time.sleep(5)
+    button = len(browser.find_elements_by_class_name("btn-primary"))
+    assert button > 0, "I can't find button"
+    time.sleep(10)
+
