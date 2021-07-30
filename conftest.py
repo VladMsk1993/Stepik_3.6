efe3a8a38212858192c9d7bfb68cf881ad861654
VladMSK1993 <sharvladrus@gmail.com>
2021-07-30 15:46:51 +0300

    Доделананный файл с рабочим кодом.

--- conftest.py
+++ conftest.py
@@ -3,32 +3,28 @@ from selenium import webdriver
 import pytest
 
 
-@pytest.fixture(scope="function")
-def language():
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
-
-
-
-
 def pytest_addoption(parser):
-    parser.addoption('--language_name', action='store', default="En",
-                     help="Choose language: en or fr")
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="Choose language: ru, en, ... (etc.)")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    language_name = request.config.getoption("language_name")
-    browser = None
-    if language_name == "en":
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+    if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-    elif language_name == "fr":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
-        raise pytest.UsageError("--language_name should be en or fr")
+        print("Browser {} still is not implemented".format(browser_name))
     yield browser
     print("\nquit browser..")
     browser.quit()
